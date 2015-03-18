43bf61f5bb8fc134d135a7e6887c7974f5530bd4
vika L <vika.lapsar@gmail.com>
2015-03-18 21:30:16 +0200

    some work with Harmonization

--- music/main.hs
+++ music/main.hs
@@ -6,47 +6,66 @@ maxPosition = 7
 notes = [("C",0,0), ("Cb",0,11),("C#",0,1),("D",1,2),("Db",1,1),("D#",1,3),("E",2,4),("Eb",2,3),("E#",2,5),("F",3,5), ("Fb",3,4), ("F#",3,6), ("G",4,7),("Gb",4,6),("G#",4,8), ("A",5,9),("Ab",5,8),("A#",5,10),("B",6,11),("Bb",6,10),("B#",6,12)]
 dur_intervals  = [2,2,1,2,2,2,1]
 moll_intervals = [2,1,2,2,1,2,2]
+
+
+-- ********* Harmonization *********
+-- Music Theory: harmonization of major/minor scales with Triads, Seventh chords etc
+-- http://en.wikipedia.org/wiki/Harmonization 
+
+-- harmonizeScale note isDur numNotes = 
+-- [(snd3 (searchNoteByName "C")), (snd3 (searchNoteByName "C")) + 1..(snd3 (searchNoteByName "C")) + 3 -1]
+
+findNoteByLevelTonality :: [([Char], Integer, Integer)] -> Integer -> [([Char], Integer, Integer)]
+findNoteByLevelTonality tonality level  = [n| n <- tonality, (snd3 n) == level] -- сделай из меня лямбду 
+
+buildAccordLevels noteName count = 
+	let start = (snd3 (searchNoteByName noteName))
+	in map transposePosition [start, start + 2.. start + 2 * count - 1]
  
--- get Tonality functions
+generateAccord note tonic count =
+	let tonality = buildTonality tonic True
+	in map (findNoteByLevelTonality tonality) (buildAccordLevels note count)
+ 
+-- ********* Tonality functions *********
 
 --  Sample :
---  Main> getTonalityShort "A" True
+--  Main> buildTonalityShort "A" True
 --   ["A","B","C#","D","E","F#","G#"]
---  Main> getTonalityShort "Afdg" True
+--  Main> buildTonalityShort "Afdg" True
 --   ["Bad input"]
 
-getTonalityShort :: [Char] -> Bool -> [[Char]]
-getTonalityShort noteName isDur = map frst (getTonality noteName isDur)
+buildTonalityShort :: [Char] -> Bool -> [[Char]]
+buildTonalityShort noteName isDur = map frst (buildTonality noteName isDur)
 
 --  Sample:
---  Main> getTonality "C" True
+--  Main> buildTonality "C" True
 --   [("C",0,0),("D",1,2),("E",2,4),("F",3,5),("G",4,7),("A",5,9),("B",6,11)]
 
-getTonality :: [Char] -> Bool -> [([Char], Integer, Integer)]
-getTonality noteName isDur 
+buildTonality :: [Char] -> Bool -> [([Char], Integer, Integer)]
+buildTonality noteName isDur 
     | null [(note) | note <- notes, frst note == noteName] = [("Bad input", 0, 0)]
-	| otherwise = map searchNoteByCharacteristic (getRowValues (searchNoteByName noteName) isDur)
+	| otherwise = map searchNoteByCharacteristic (buildRowNotesDataSeq (searchNoteByName noteName) isDur)
 		
 		
-getRowValues ::  ([Char], Integer, Integer)  -> Bool -> [(Integer, Integer)]
-getRowValues note isDur =  
+buildRowNotesDataSeq ::  ([Char], Integer, Integer)  -> Bool -> [(Integer, Integer)] -- 
+buildRowNotesDataSeq note isDur =  
 	let intervals  
 		| isDur = dur_intervals
 		| otherwise = moll_intervals
-	in  zip (getLevelValuesTransposed (snd3 note)) (getToneValues (last3 note) intervals)
+	in  zip (getLevelValues (snd3 note)) (getToneValues (last3 note) intervals)
 
--- generate tones and levels lists for Tonality
+-- ********* generate tones and levels lists for Tonality *********
 
 getToneValues :: Integer -> [Integer] -> [Integer]
-getToneValues num list = summ num list
+getToneValues num list = map transposeTone (summ num list)
 
 getLevelValues :: Integer -> [Integer]
-getLevelValues num =  [num, num + 1.. num + maxPosition - 1]
+getLevelValues num = map transposePosition (getLevelValuesRaw num) -- out ex: [5,6,0,1,2,3,4]
 
-getLevelValuesTransposed :: Integer -> [Integer]
-getLevelValuesTransposed num = map transposePosition (getLevelValues num)
+getLevelValuesRaw :: Integer -> [Integer]   
+getLevelValuesRaw num =  [num, num + 1.. num + maxPosition - 1]    -- out ex: [5,6,7,8,9,10,11]
 
--- TRANPOSING TONES and LEVELS
+-- ********* TRANPOSING TONES and LEVELS *********
 -- tone 14 ~ 2, level 8 ~ 1
 
 transposePosition :: Integer -> Integer
@@ -61,18 +80,18 @@ transpose' x value
 	| x < value = x
 	| otherwise = x `mod` value
 
--- LIST SEARCH
+-- ********* NOTES LIST SEARCH *********
 searchNoteByName :: [Char] -> ([Char], Integer, Integer)
 searchNoteByName name = [a | a <- notes, (frst a == name)] !! 0
 
 searchNoteByCharacteristic :: (Integer, Integer) -> ([Char], Integer, Integer)
 searchNoteByCharacteristic chars = [note | note <- notes, (fst chars) == (snd3 note), (snd chars) == (last3 note)] !! 0
 
--- LIST UTILS
+-- ********* LIST UTILS *********
 	
 summ :: Integer -> [Integer] -> [Integer]
 summ num [] = [num]
-summ num (x:xs) = num : summ (transposeTone (num + x)) xs
+summ num (x:xs) = num : summ (num + x) xs
 	
 frst :: (a, b, c) -> a
 frst (a,_,_) = a
